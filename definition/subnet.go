3703fed29d906559195e0271da2db3a021d4ca38
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2022-03-28 17:30:37 -0400

    adding version metadata to vms

--- definition/subnet.go
+++ definition/subnet.go
@@ -1,12 +1,15 @@
 package definition
 
-import "github.com/ava-labs/avalanchego/chains"
+import (
+	"github.com/ava-labs/avalanchego/chains"
+	"github.com/ava-labs/avalanchego/version"
+)
 
 type Subnet interface {
 	Plugin
 
 	// VMs that are required to process this subnet
-	VMs() []VM
+	VMs() map[VM]version.Version
 	// SubnetConfig describes parameters for the subnet.
 	SubnetConfig() *chains.SubnetConfig
 }
