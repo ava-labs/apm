12d6ce6f68129d538d24412b0db08a1c5889f0c1
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2022-06-03 14:22:21 -0400

    more tests

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -59,9 +59,12 @@ func TestDatabase_Put(t *testing.T) {
 			name: "no error",
 			err:  nil,
 		},
-
 		{
-			name: "error",
+			name: "error on marshal",
+			err:  fmt.Errorf("foo"),
+		},
+		{
+			name: "error on put",
 			err:  fmt.Errorf("foo"),
 		},
 	}
@@ -87,28 +90,45 @@ func TestDatabase_Put(t *testing.T) {
 	}
 }
 
+// TODO verify type of error?
 func TestDatabase_Get(t *testing.T) {
-	bytes, err := yaml.Marshal("foo")
+	type value struct {
+		Foo string  `yaml:"foo"`
+		Bar float32 `yaml:"bar"`
+	}
+
+	bytes, err := yaml.Marshal(value{"foo", 3.14})
 	assert.NoError(t, err)
 
 	tests := []struct {
-		name  string
-		bytes []byte
-		value string
-		err   error
+		name      string
+		bytes     []byte
+		value     value
+		dbErr     error
+		shouldErr bool
 	}{
 		{
-			name:  "no error",
-			bytes: bytes,
-			value: "foo",
-			err:   nil,
+			name:      "no error",
+			bytes:     bytes,
+			value:     value{"foo", 3.14},
+			dbErr:     nil,
+			shouldErr: false,
 		},
 
 		{
-			name:  "error",
-			bytes: nil,
-			value: "",
-			err:   fmt.Errorf("foo"),
+			name:      "error from db",
+			bytes:     nil,
+			value:     value{},
+			dbErr:     fmt.Errorf("foo"),
+			shouldErr: true,
+		},
+
+		{
+			name:      "error from deserialization",
+			bytes:     []byte("wrong"),
+			value:     value{},
+			dbErr:     nil,
+			shouldErr: true,
 		},
 	}
 
@@ -118,15 +138,19 @@ func TestDatabase_Get(t *testing.T) {
 			db := mocks.NewMockDatabase(ctrl)
 
 			key := []byte("key")
-			db.EXPECT().Get(key).Return(bytes, test.err)
+			db.EXPECT().Get(key).Return(test.bytes, test.dbErr)
 
-			storage := &Database[string]{
+			storage := &Database[value]{
 				db: db,
 			}
 
 			value, err := storage.Get(key)
 			assert.Equal(t, test.value, value)
-			assert.Equal(t, test.err, err)
+			if test.shouldErr {
+				assert.Error(t, err)
+			} else {
+				assert.Nil(t, err)
+			}
 		})
 	}
 }
